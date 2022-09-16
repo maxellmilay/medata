aa3d8314063e8d66b43f40f11bef810c2e3c6b38
Maxell Milay <milaymaxell@gmail.com>
2022-09-17 02:53:26 +0800

    [UI] layout css fixes

--- client/src/pages/Dashboard/MediaInfo/index.tsx
+++ client/src/pages/Dashboard/MediaInfo/index.tsx
@@ -1,11 +1,13 @@
-import MediaItemDetails from "./MediaItemDetails"
-import ProfileOptions from "./ProfileOptions"
-
 function MediaInfo() {
     return (
-        <div className="flex flex-col h-full w-3/5 grow">
-            <MediaItemDetails />
-        </div>
+        <div className="flex-col grow w-3/5 p-10 overflow-auto media-item border scrollbar">
+            <h2 className="w-full text-2xl font-bold mb-3 open-sans">Prepare offers for new clients</h2>
+            <p className="w-full text-xs mb-14 open-sans">10 Sep, 2022</p>
+            <p className="w-full mb-10 open-sans">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p className="w-full mb-10 open-sans">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p className="w-full mb-10 open-sans">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p className="w-full mb-10 open-sans">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div >
     )
 }
 
